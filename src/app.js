a0821da192fe2ac725263203e24a16d03c6f56fb
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-05-04 18:49:24 +0100

    setyp react with routing and bootsrap

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-const app = async () => '#BuildforSDG';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
-export default app;
+function App() {
+  return (
+      <Switch>
+          <Route exact path="/" component={Home} />
+    </Switch>
+  );
+}
+
+export default App;
