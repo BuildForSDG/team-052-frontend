58e4af77a224b5986e6a5b5d0b92bd9d03f4fb64
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-05-08 02:48:12 +0100

    add card bc of shadow merging

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -13,52 +13,40 @@ const Reports = () => {
       <Banner />
       <div className="container">
         <ReportsContainer>
-          <div className="row mt-2">
-            <div className="col-6" style={{ width: '18rem' }}>
-              <div className="reports-image m-4">
-                <img src={AccidentImage} className="card-img-top" />
-              </div>
-              <div className="card-body pt-2 shadow">
-                <div style={{ display: 'flex' }}>
-                  <p>STATUS</p>: <span className="text-info">ENROUTE</span>
-                  <p className="text-right ml-auto">SEVERITY: </p>
-                  <span className="text-danger">HEAVY</span>
+          <div className="row">
+            <div className="col-md-6" style={{ width: '18rem' }}>
+              <div className="card shadow">
+                <div className="reports-image m-4">
+                  <img src={AccidentImage} className="card-img-top" />
                 </div>
-                <p className="card-title text-center">
-                  <strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nostrum!</strong>
-                </p>
+                <div className="card-body pt-2">
+                  <div style={{ display: 'flex' }}>
+                    <p>STATUS</p>: <span className="text-info">ENROUTE</span>
+                    <p className="text-right ml-auto">SEVERITY: </p>
+                    <span className="text-danger">HEAVY</span>
+                  </div>
+                  <p className="card-title text-center">
+                    <strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nostrum!</strong>
+                  </p>
+                </div>
               </div>
             </div>
 
-            <div className="col-6" style={{ width: '18rem' }}>
-              <div className="reports-image m-4">
-                <img src={AccidentImage} className="card-img-top" />
-              </div>
-              <div className="card-body pt-2 shadow">
-                <div style={{ display: 'flex' }}>
-                  <p>STATUS</p>: <span className="text-info">ENROUTE</span>
-                  <p className="text-right ml-auto">SEVERITY: </p>
-                  <span className="text-danger">HEAVY</span>
+            <div className="col-md-6" style={{ width: '18rem' }}>
+              <div className="card shadow">
+                <div className="reports-image m-4">
+                  <img src={AccidentImage} className="card-img-top" />
                 </div>
-                <p className="card-title text-center">
-                  <strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nostrum!</strong>
-                </p>
-              </div>
-            </div>
-
-            <div className="col-6" style={{ width: '18rem' }}>
-              <div className="reports-image m-4">
-                <img src={AccidentImage} className="card-img-top" />
-              </div>
-              <div className="card-body pt-2 shadow">
-                <div style={{ display: 'flex' }}>
-                  <p>STATUS</p>: <span className="text-info">ENROUTE</span>
-                  <p className="text-right ml-auto">SEVERITY: </p>
-                  <span className="text-danger">HEAVY</span>
+                <div className="card-body pt-2">
+                  <div style={{ display: 'flex' }}>
+                    <p>STATUS</p>: <span className="text-info">ENROUTE</span>
+                    <p className="text-right ml-auto">SEVERITY: </p>
+                    <span className="text-danger">HEAVY</span>
+                  </div>
+                  <p className="card-title text-center">
+                    <strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nostrum!</strong>
+                  </p>
                 </div>
-                <p className="card-title text-center">
-                  <strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nostrum!</strong>
-                </p>
               </div>
             </div>
           </div>
