080ec38a259f4230b86f7b640e046cc9f83c9a92
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-05-14 16:20:42 +0100

    set up ts

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Reports from './components/Reports';
 import CreateReport from './components/CreateReport';
