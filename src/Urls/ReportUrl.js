c7335812d4cbcc1eae27e1ce4fa27b0569670b04
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-05-16 22:24:20 +0100

    add proxy for local bckend url

--- src/Urls/ReportUrl.js
+++ src/Urls/ReportUrl.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 import { APP_URL } from './BaseUrl';
 
-export const GET_ALL_REPORTS_URL = `${APP_URL}/reports`;
+export const GET_ALL_REPORTS_URL = `/reports`;
