f32be3f1beda297308762a552bb47d16ee6875a0
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-06-12 04:50:08 +0100

    function

--- src/reducers/reports/reportsReducer.js
+++ src/reducers/reports/reportsReducer.js
@@ -1,6 +1,6 @@
 import * as types from '../../constants/ReportTypes';
 
-export const reportsReducer = (state, action) => {
+export function reportsReducer(state, action) {
   const { type, payload } = action;
   switch (type) {
     case types.LOADING_STARTS:
@@ -23,6 +23,6 @@ export const reportsReducer = (state, action) => {
     default:
       return state;
   }
-};
+}
 
 export default reportsReducer;
