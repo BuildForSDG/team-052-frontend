1a522add2c6029eb6baf4f45cf5deaa60966d8b2
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-06-29 12:03:09 +0100

    fixes

--- src/components/reports/CreateReport.tsx
+++ src/components/reports/CreateReport.tsx
@@ -5,7 +5,7 @@ import { CreateReportProps } from '../../interfaces/CreateReportProps';
 import { ToastContainer } from 'react-toastify';
 import Navigation from '../layouts/v2/Navigation';
 // import { ReportsProps } from '../interfaces/ReportsProps.interface';
-import { ReportsProps } from '../../interfaces/ReportProps.interface';
+import { ReportProps } from '../../interfaces/ReportProps.interface';
 
 // type CreateReportType = {
 //   name: string;
@@ -18,7 +18,7 @@ import { ReportsProps } from '../../interfaces/ReportProps.interface';
 //   report: CreateReportType[];
 // };
 
-const CreateReport: FC = ({ history }) => {
+const CreateReport: FC<ReportProps> = ({ history }) => {
   const { report, createReport, onReportChange, onFileChange, file, loadingReport } = useContext(AppContext);
   // const { onChange } = useReports();
   const { location, name, description, loaded } = report;
--- src/interfaces/ReportsProps.interface.tsx
+++ src/interfaces/ReportsProps.interface.tsx
@@ -1,13 +1,18 @@
-import { RouteComponentProps } from 'react-router-dom';
-// export interface ReportsProps  {
-//   name: string;
-//   numberType?: number;
-//   report: {
-//     name: string;
-//     description: string;
-//     location: string;
-//   };
+// import { RouteComponentProps } from 'react-router-dom';
+// // export interface ReportsProps  {
+// //   name: string;
+// //   numberType?: number;
+// //   report: {
+// //     name: string;
+// //     description: string;
+// //     location: string;
+// //   };
+// // }
+// export interface ReportsProps extends RouteComponentProps<any> {
+//     /* other props  */
 // }
-export interface ReportsProps extends RouteComponentProps<any> {
-    /* other props  */
+
+export interface ReportsProps {
+  name: string;
+  numberType?: number;
 }
